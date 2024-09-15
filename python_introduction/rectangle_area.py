26b033c0b890a75fce5aa7c4ba9929fc29c96c00
David Junior Phiri <davidphiri242@gmail.com>
2024-09-15 22:23:48 +0200

    Add files via upload
    added new files

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,5 @@
+#calculates area of rectangle
+length = 10
+width = 5
+area = length * width      #area formula
+print("The area of the rectangle is:",area)
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,5 @@
+principal = int(1000)      #represents $1000
+rate = int(0.05)           #represents 5% annual interest rate
+time = int(3)              #represents 3 years 
+interest = principal * rate * time  #interest rate formula
+print ("The simple interest is ",interest)
