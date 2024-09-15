02fe2977db66a48abc2cd05dbd018a3c5df869fe
David Junior Phiri <davidphiri242@gmail.com>
2024-09-15 23:47:13 +0200

    Add files via upload

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,5 @@
+principal = 1000    #represents $1000
+rate = 0.05          #represents 5% annual interest rate
+time = 3             #represents 3 years 
+interest = principal * rate * time  #interest rate formula
+print ("The simple interest is ",interest)
