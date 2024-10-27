c7d7bdc8a3a2047216b6326b5ce1193a5187f58e
David Junior Phiri <davidphiri242@gmail.com>
2024-10-27 17:54:31 +0200

    Add files via upload

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+#Function performs basic arithmetic operations
+def perform_operation(num1: float, num2:float, operation: str):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero is not allowed."
+        return num1 / num2
+    else: 
+        return "Error: Please choose 'add', 'subtract', 'multiply', or 'divide'."
+    
